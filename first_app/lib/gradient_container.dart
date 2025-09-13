9099eacbddf9ded7b36dd4e1a988c420bb24a17c
Daniel Chavarria <danielcoto2004@gmail.com>
2025-09-13 01:14:22 -0600

    feat: add current progress on first_app

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,3 +1,4 @@
+import 'package:first_app/dice_roller.dart';
 import 'package:flutter/material.dart';
 import 'package:first_app/styled_text.dart';
 
@@ -5,12 +6,9 @@ const startAlignment = Alignment.topRight;
 const endAlignment = Alignment.bottomLeft;
 
 class GradientContainer extends StatelessWidget {
-  GradientContainer({super.key, required this.colors});
+  const GradientContainer({super.key, required this.colors});
 
   final List<Color> colors;
-  var activeDiceImage = 'assets/images/dice-1.png';
-
-  void rollDice() {}
 
   @override
   Widget build(context) {
@@ -22,24 +20,7 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(activeDiceImage, width: 200),
-            SizedBox(height: 20),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                padding: EdgeInsets.only(top: 20),
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(fontSize: 28),
-              ),
-              child: const Text('Roll Dice'),
-            ),
-          ],
-        ),
-      ),
+      child: Center(child: DiceRoller()),
     );
   }
 }
