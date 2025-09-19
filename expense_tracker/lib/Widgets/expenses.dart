b222e76421061b518e07257f63dadd2f8ab57312
Daniel Chavarria <danielcoto2004@gmail.com>
2025-09-19 17:20:05 -0600

    App Expense Tracker completed

--- expense_tracker/lib/Widgets/expenses.dart
+++ expense_tracker/lib/Widgets/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/Widgets/chart/chart.dart';
 import 'package:expense_tracker/Widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/Widgets/new_expense.dart';
 import 'package:expense_tracker/models/expense.dart';
@@ -30,6 +31,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+      useSafeArea: true,
       isScrollControlled: true,
       context: context,
       builder: (ctx) => NewExpense(onAddExpense: _addExpense),
@@ -67,6 +69,8 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
     Widget mainContent = const Center(
       child: Text('No expenses found. Start adding some!'),
     );
@@ -88,12 +92,19 @@ class _ExpensesState extends State<Expenses> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          const Text("The chart"),
-          Expanded(child: mainContent),
-        ],
-      ),
+      body: width < 600
+          ? Column(
+              children: [
+                Chart(expenses: _registerExpenses),
+                Expanded(child: mainContent),
+              ],
+            )
+          : Row(
+              children: [
+                Expanded(child: Chart(expenses: _registerExpenses)),
+                Expanded(child: mainContent),
+              ],
+            ),
     );
   }
 }
